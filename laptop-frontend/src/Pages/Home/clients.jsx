e7eb391890e9b7828e5df28781e07fc3d56b4905
jeya-jesina <jeyajesina@gmail.com>
2026-07-16 11:46:58 +0530

    home pages

---
+++ laptop-frontend/src/Pages/Home/clients.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function clients() {
+  return (
+    <div>clients</div>
+  )
+}
---
+++ laptop-frontend/src/Pages/Home/deals.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function deals() {
+  return (
+    <div>deals</div>
+  )
+}
